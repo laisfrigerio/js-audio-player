04542eb9d64be6f0bc3a053ce71fdf4969c2005a
Lais Frigério <laaisfrigerio@gmail.com>
2021-01-23 22:07:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center">
+  <a href="https://github.com/laisfrigerio/js-audio-player">
+    <img src="./images/logo.png" alt="Logo">
+  </a>
+</p>
+
+# :copyright: Pl<|yer
+
+An audio player made with pure javascript :blue_heart:
+
+# :camera: Screenshots
+
+![Desktop](https://raw.githubusercontent.com/laisfrigerio/js-audio-player/master/screenshots/desktop.png)
+
+![Mobile](https://raw.githubusercontent.com/laisfrigerio/js-audio-player/master/screenshots/mobile.png)
+
+## 🛠️ Stack
+
+- HTML/PUG
+- CSS/SASS
+- Javascript
+- Gulp (To compile sass/pug/js)
+- Fontawesome icons
+- Create logo [here](https://fontmeme.com/netflix-font/)
+
+## :woman: Author
+
+[@laisfrigerio](https://github.com/laisfrigerio/)
+
+## 📄 License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ resources/scripts/audios.js
@@ -0,0 +1,106 @@
+export default [
+    {
+        album: '21',
+        author: 'Adele',
+        year: '2011',
+        track: [
+            {
+                title: 'Rolling in the deep',
+                image: null,
+                isSingle: true,
+                song: 'rolling-in-the-deep-adele.mp3'
+            },
+            {
+                title: 'Rumor has it',
+                image: null,
+                isSingle: true,
+                song: 'rumor-has-it-adele.mp3'
+            },
+            {
+                title: 'Set fire to the rain',
+                image: null,
+                isSingle: true,
+                song: 'set-fire-to-the-rain-adele.mp3'
+            },
+            {
+                title: 'Someone like you',
+                image: null,
+                isSingle: true,
+                song: 'someone-like-you-adele.mp3'
+            }
+        ]
+    },
+    {
+        album: '25',
+        author: 'Adele',
+        year: '2016',
+        track: [
+            {
+                title: 'Hello',
+                image: null,
+                isSingle: true,
+                song: 'adele-hello.mp3'
+            },
+            {
+                title: 'All I ask',
+                image: null,
+                isSingle: false,
+                song: 'all-i-ask-adele.mp3'
+            },
+            {
+                title: 'I miss you',
+                image: null,
+                isSingle: false,
+                song: 'i-miss-you-adele.mp3'
+            },
+            {
+                title: 'Love in the dark',
+                image: null,
+                isSingle: false,
+                song: 'love-in-the-dark-adele.mp3'
+            },
+            {
+                title: 'Million Years ago',
+                image: null,
+                isSingle: false,
+                song: 'million-years-ago-adele.mp3'
+            },
+            {
+                title: 'Remedy',
+                image: null,
+                isSingle: false,
+                song: 'remedy-adele.mp3'
+            },
+            {
+                title: 'River lea',
+                image: null,
+                isSingle: false,
+                song: 'river-lea-adele.mp3'
+            },
+            {
+                title: 'Send My Love (to Your new Lover)',
+                image: null,
+                isSingle: true,
+                song: 'send-my-love-to-your-new-lover-adele.mp3'
+            },
+            {
+                title: 'Sweestest devotion',
+                image: null,
+                isSingle: false,
+                song: 'sweetest-devotion-adele.mp3'
+            },
+            {
+                title: 'Water under the bridge',
+                image: null,
+                isSingle: true,
+                song: 'water-under-the-bridge-adele.mp3'
+            },
+            {
+                title: 'When we are young',
+                image: null,
+                isSingle: true,
+                song: 'when-we-were-young-adele.mp3'
+            },
+        ]
+    }
+];
